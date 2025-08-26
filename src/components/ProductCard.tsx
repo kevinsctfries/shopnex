6c1b5df438f7f2268b627a894ddfe8f3e988ea4e
Kevin Fries <kevinsctfries@gmail.com>
2025-08-26 14:13:53 -0400

    feat: add "Add to Cart" functionality to ProductCard

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -1,6 +1,9 @@
+"use client";
+
 import Link from "next/link";
 import Image from "next/image";
 import styles from "./ProductCard.module.css";
+import { useCart } from "@/context/CartContext";
 
 interface ProductCardProps {
   id: number;
@@ -15,6 +18,12 @@ export default function ProductCard({
   price,
   image,
 }: ProductCardProps) {
+  const { addToCart } = useCart();
+
+  const handleAddToCart = () => {
+    addToCart({ id, title, price, image, quantity: 1 });
+  };
+
   return (
     <div className={styles.productCard}>
       <Image
@@ -28,7 +37,9 @@ export default function ProductCard({
         <h3>{title}</h3>
         <p>${price.toFixed(2)}</p>
         <Link href={`/products/${id}`}>See Details</Link>
-        <button className="button-accent">Add to cart</button>
+        <button className="button-accent" onClick={handleAddToCart}>
+          Add to cart
+        </button>
         <button className="button-accent">Buy Now</button>
       </div>
     </div>
